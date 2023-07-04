238f82dfa0f628b1c72eb6ad878aa1f81c8bebc7
SeokHyeon-Eom <eom2244@kookmin.ac.kr>
2023-07-04 17:17:08 +0900

    add: select date feature

--- traflix-web/src/components/searchbarComponent/index.tsx
+++ traflix-web/src/components/searchbarComponent/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useRootData from '../../hooks/useRootData';
 import stylesDesktopDefault from './DesktopDefault.module.scss';
 import Form from 'react-bootstrap/Form';
@@ -9,43 +9,115 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 const mockStation: string[] = [
   '길음역',
   '종각역',
-  '동인천역',
   '불광역',
   '시청역',
+  '동인천역',
   '용산역',
+  '역1',
+  '역2',
+  '역3',
+  '역4',
+  '역5',
+  '역6',
+  '역7',
+  '역8',
+  '역9',
+  '역10',
+  '역11',
+  '역12',
+  '역13',
+  '역14',
+  '역15',
+  '역16',
+  '역17',
+  '역18',
+  '역19',
+  '역20',
 ];
 
-const SearchbarComponent = () => {
+export interface PropsSearchbarComponent {
+  stationList: string[];
+}
+
+const SearchbarComponent: React.FunctionComponent<PropsSearchbarComponent> = ({
+  stationList,
+}) => {
   const { screenClass } = useRootData(({ appStore }) => ({
     screenClass: appStore.screenClass.get(),
   }));
   const isDesktop = screenClass === 'xl';
 
+  if (stationList == null) stationList = mockStation;
+
+  const [start, setStart] = useState(stationList[0]);
+  const [destination, setDestination] = useState(stationList[0]);
+  const [startDate, setStartDate] = useState('');
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = ('0' + (1 + date.getMonth())).slice(-2);
+  const day = ('0' + date.getDate()).slice(-2);
+  const today = year + '-' + month + '-' + day;
+
+  const searchPath = () => {
+    if (startDate === '') alert('날짜를 골라주세요');
+    else if (start === destination)
+      alert('출발지와 도착지를 다르게 설정해주세요');
+    else
+      alert(`start: ${start}, destination: ${destination} date: ${startDate}`);
+  };
+
   const styles = isDesktop ? stylesDesktopDefault : stylesDesktopDefault;
-  // className={styles.main}
   return (
-    <div>
-      <Form className={styles.search}>
-        <div className={styles.select}>
-          <FloatingLabel controlId="floatingSelectS" label="출발역">
-            <Form.Select aria-label="stationSelect" placeholder="출발역">
+    <div className={styles.main}>
+      <Form onSubmit={searchPath} className={styles.search}>
+        <div className={styles.selectStations}>
+          <FloatingLabel
+            controlId="labelStart"
+            label="출발역"
+            className={styles.selectLabel}
+          >
+            <Form.Select
+              aria-label="stationSelect"
+              value={start}
+              onChange={(e) => setStart(e.target.value)}
+              className={styles.selectButton}
+            >
+              {mockStation.map((station) => (
+                <option value={station}>{station}</option>
+              ))}
+            </Form.Select>
+          </FloatingLabel>
+          <FloatingLabel
+            controlId="labelDestination"
+            label="도착역"
+            className={styles.selectLabel}
+          >
+            <Form.Select
+              aria-label="destinationSelect"
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}
+              className={styles.selectButton}
+            >
               {mockStation.map((station) => (
                 <option value={station}>{station}</option>
               ))}
             </Form.Select>
           </FloatingLabel>
         </div>
-        <div className={styles.select}>
-          <FloatingLabel controlId="floatingSelectD" label="도착역">
-            <Form.Select aria-label="destinationSelect">
-              {mockStation.map((station) => (
-                <option value={station}>{station}</option>
-              ))}
-            </Form.Select>
-          </FloatingLabel>
-        </div>
-        <Button variant="primary" type="submit" className={styles.button}>
-          Submit
+        <Form.Control
+          type="date"
+          min={today}
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+          className={styles.calender}
+        />
+        <Button
+          type="submit"
+          variant="outline-success"
+          className={styles.button}
+        >
+          검색
         </Button>
       </Form>
     </div>
