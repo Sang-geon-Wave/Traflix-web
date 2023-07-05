44b0e3b2efa0ac109d72d84880f02d0b4b89a8dd
SeokHyeon-Eom <eom2244@kookmin.ac.kr>
2023-07-05 11:13:22 +0900

    refactor: modify mock data path

--- traflix-web/src/App.tsx
+++ traflix-web/src/App.tsx
@@ -3,13 +3,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/Landing';
 import MockPage from './pages/MockPage';
 import SearchbarComponent from './components/searchbarComponent';
+import mockStation from './assets/stting/seachbarComponent/testDate';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/mock" element={<MockPage />} />
       <Route path="/" element={<LandingPage />} />
-      <Route path="/testSearchBar" element={<SearchbarComponent />} />
+      <Route
+        path="/testSearchBar"
+        element={<SearchbarComponent stationList={mockStation} />}
+      />
     </Routes>
   </Router>
 );
--- traflix-web/src/components/searchbarComponent/index.tsx
+++ traflix-web/src/components/searchbarComponent/index.tsx
@@ -6,35 +6,6 @@ import Button from 'react-bootstrap/Button';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 // import stylesMobileDefault from './MobileDefault.module.scss';
 
-const mockStation: string[] = [
-  '길음역',
-  '종각역',
-  '불광역',
-  '시청역',
-  '동인천역',
-  '용산역',
-  '역1',
-  '역2',
-  '역3',
-  '역4',
-  '역5',
-  '역6',
-  '역7',
-  '역8',
-  '역9',
-  '역10',
-  '역11',
-  '역12',
-  '역13',
-  '역14',
-  '역15',
-  '역16',
-  '역17',
-  '역18',
-  '역19',
-  '역20',
-];
-
 export interface PropsSearchbarComponent {
   stationList: string[];
 }
@@ -46,8 +17,6 @@ const SearchbarComponent: React.FunctionComponent<PropsSearchbarComponent> = ({
     screenClass: appStore.screenClass.get(),
   }));
   const isDesktop = screenClass === 'xl';
-
-  if (stationList == null) stationList = mockStation;
 
   const [start, setStart] = useState(stationList[0]);
   const [destination, setDestination] = useState(stationList[0]);
@@ -83,7 +52,7 @@ const SearchbarComponent: React.FunctionComponent<PropsSearchbarComponent> = ({
               onChange={(e) => setStart(e.target.value)}
               className={styles.selectButton}
             >
-              {mockStation.map((station) => (
+              {stationList.map((station) => (
                 <option value={station}>{station}</option>
               ))}
             </Form.Select>
@@ -99,7 +68,7 @@ const SearchbarComponent: React.FunctionComponent<PropsSearchbarComponent> = ({
               onChange={(e) => setDestination(e.target.value)}
               className={styles.selectButton}
             >
-              {mockStation.map((station) => (
+              {stationList.map((station) => (
                 <option value={station}>{station}</option>
               ))}
             </Form.Select>
