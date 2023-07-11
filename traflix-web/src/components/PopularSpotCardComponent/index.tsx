d6fc1b0258373110d0d76a0f624fda02351666d8
parkjs82 <54925443+parkjs82@users.noreply.github.com>
2023-07-11 19:46:34 +0900

    del flip

--- traflix-web/src/components/PopularSpotCardComponent/index.tsx
+++ traflix-web/src/components/PopularSpotCardComponent/index.tsx
@@ -1,47 +1,34 @@
 import React from 'react';
 import useRootData from '../../hooks/useRootData';
 import stylesDesktopDefault from './DesktopDefault.module.scss';
-import { Card } from 'react-bootstrap';
 
 export interface PropsPopularSpotCardComponent {
   imgUrl: string;
-  width?: number;
-  height?: number;
-  handleClick?: () => any;
+  place: string;
+  addr: string;
+  info: string;
 }
 
 const PopularSpotCardComponent: React.FunctionComponent<
   PropsPopularSpotCardComponent
-> = ({ imgUrl, width = 579, height = 384, handleClick }) => {
+> = ({ imgUrl, place, addr, info }) => {
   const { screenClass } = useRootData(({ appStore }) => ({
     screenClass: appStore.screenClass.get(),
   }));
   const isDesktop = screenClass === 'xl';
 
   const styles = isDesktop ? stylesDesktopDefault : stylesDesktopDefault;
-  const containerStyles = {
-    width: width,
-    height: height,
-  };
 
   return (
-    <div
-      className={styles.flip_container}
-      style={containerStyles}
-      onClick={handleClick}
-    >
-      <div className={styles.flipper} style={containerStyles}>
-        <div className={styles.front} style={containerStyles}>
-          <Card style={containerStyles} className="border-0">
-            <Card.Img variant="top" src={imgUrl} />
-          </Card>
+    <div className={styles.card}>
+      <li>
+        <img src={imgUrl} />
+        <div className={styles.caption}>
+          <h2>{place}</h2>
+          <p>{addr}</p>
+          <p>{info}</p>
         </div>
-        <div className={styles.back} style={containerStyles}>
-          <Card style={containerStyles} className="border-0">
-            {/*TODO: You can customize this section.*/}
-          </Card>
-        </div>
-      </div>
+      </li>
     </div>
   );
 };
--- traflix-web/src/pages/TestPopularSpotCardPage/index.tsx
+++ traflix-web/src/pages/TestPopularSpotCardPage/index.tsx
@@ -6,6 +6,8 @@ import stylesDesktopDefault from './DesktopDefault.module.scss';
 import stylesMobileDefault from './MobileDefault.module.scss';
 import svg_test1 from '../../assets/images/popular_spot_1.svg';
 import svg_test2 from '../../assets/images/popular_spot_2.svg';
+import dong from '../../assets/images/dongdaemun.png';
+import castle from '../../assets/images/castle.png';
 
 const TestPopularSpotCardPage = () => {
   const { screenClass } = useRootData(({ appStore }) => ({
@@ -17,39 +19,39 @@ const TestPopularSpotCardPage = () => {
 
   const responsive = {
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
+      breakpoint: { max: 3000, min: 1100 },
       items: 3,
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
+      breakpoint: { max: 1099, min: 750 },
       items: 2,
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 749, min: 0 },
       items: 1,
     },
   };
   return (
-    <>
-      <Carousel
-        responsive={responsive}
-        itemClass={styles.carousel_item}
-        showDots={true}
-      >
-        <div>
-          <PopularSpotCardComponent imgUrl={svg_test1} />
-        </div>
-        <div>
-          <PopularSpotCardComponent imgUrl={svg_test2} />
-        </div>
-        <div>
-          <PopularSpotCardComponent imgUrl={svg_test1} />
-        </div>
-        <div>
-          <PopularSpotCardComponent imgUrl={svg_test2} />
-        </div>
-      </Carousel>
-    </>
+    <Carousel
+      responsive={responsive}
+      itemClass={styles.carousel_item}
+      showDots={true}
+    >
+      <PopularSpotCardComponent imgUrl={dong} place="1" addr="1" info="1" />
+      <PopularSpotCardComponent imgUrl={castle} place="2" addr="2" info="2" />
+      <PopularSpotCardComponent
+        imgUrl={svg_test1}
+        place="3"
+        addr="3"
+        info="3"
+      />
+      <PopularSpotCardComponent
+        imgUrl={svg_test2}
+        place="4"
+        addr="4"
+        info="4"
+      />
+    </Carousel>
   );
 };
 
