5fd1f9abc59db375dee7c75d8f709f514bb0d964
SeokHyeon-Eom <eom2244@kookmin.ac.kr>
2023-07-12 17:41:21 +0900

    refactor: mockdata name err and remove temp router

--- traflix-web/src/App.tsx
+++ traflix-web/src/App.tsx
@@ -2,18 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/Landing';
 import MockPage from './pages/MockPage';
-import SearchbarComponent from './components/searchbarComponent';
-import mockStation from './assets/stting/seachbarComponent/testDate';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/mock" element={<MockPage />} />
       <Route path="/" element={<LandingPage />} />
-      <Route
-        path="/testSearchBar"
-        element={<SearchbarComponent stationList={mockStation} />}
-      />
     </Routes>
   </Router>
 );
