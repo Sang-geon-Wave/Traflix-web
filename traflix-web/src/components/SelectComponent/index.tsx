c872ba1b7134d6712bf410a5e970087aeb3287e5
xorud127 <xorud127@kookmin.ac.kr>
2023-07-14 00:28:46 +0900

    feat: select card

--- traflix-web/src/components/SelectComponent/index.tsx
+++ traflix-web/src/components/SelectComponent/index.tsx
@@ -2,11 +2,16 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useRootData from '../../hooks/useRootData';
 import stylesDesktopDefault from './DesktopDefault.module.scss';
-import logoImg from '../../assets/images/logo.svg';
-import listImg from '../../assets/images/list.svg';
+import aeyoungImg from '../../assets/images/aeyung.jpg';
 // import stylesMobileDefault from './MobileDefault.module.scss';
 
-const SelectComponent = ({}) => {
+export interface ProbsSelectComponent {
+  options: [];
+}
+
+const SelectComponent: React.FunctionComponent<ProbsSelectComponent> = ({
+  options,
+}) => {
   const { screenClass } = useRootData(({ appStore }) => ({
     screenClass: appStore.screenClass.get(),
   }));
@@ -15,24 +20,115 @@ const SelectComponent = ({}) => {
 
   const navigate = useNavigate();
   const [isOpen, setOpen] = useState(true);
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
-  const showModal = () => {
+  const handleShowModal = () => {
     setOpen(!isOpen);
+  };
+  const handleOptionToggle = (option) => {
+    if (selectedOptions.includes(option)) {
+      setSelectedOptions(selectedOptions.filter((item) => item !== option));
+    } else {
+      setSelectedOptions([...selectedOptions, option]);
+    }
+  };
+  const handleModalClose = () => {
+    setSelectedOptions([]);
+  };
+  const handleNextButton = () => {
+    options = selectedOptions;
+    console.log(options);
+    setOpen(!isOpen);
+    setSelectedOptions([]);
   };
 
   return (
     <div>
-      <button onClick={showModal}>test</button>
+      <button onClick={handleShowModal}>test</button>
       {isOpen && (
         <div className={styles.modalBackground}>
           <div className={styles.select}>
-            <button className={styles.close} onClick={showModal}>
+            <button
+              className={styles.close}
+              onClick={() => {
+                handleShowModal();
+                handleModalClose();
+              }}
+            >
               x
             </button>
             <div className={styles.title}>선호하는 여행 취향을 골라주세요!</div>
-            <div className={styles.subTitle}>보조 타이틀</div>
-            <div className={styles.cardBox}>카드박스</div>
-            <div className={styles.buttonBox}>
+            <div className={styles.subTitle}>
+              총 <span>{selectedOptions.length}</span>/5개 선택됨
+            </div>
+            <div className={styles.cardBox}>
+              <button
+                className={
+                  selectedOptions.includes('액티비티')
+                    ? styles.cardActive
+                    : styles.card
+                }
+                onClick={() => handleOptionToggle('액티비티')}
+              >
+                <div>
+                  <img className={styles.cardImg} src={aeyoungImg}></img>
+                </div>
+                <div className={styles.cardText}>액티비티</div>
+              </button>
+              <button
+                className={
+                  selectedOptions.includes('카페')
+                    ? styles.cardActive
+                    : styles.card
+                }
+                onClick={() => handleOptionToggle('카페')}
+              >
+                <div>
+                  <img className={styles.cardImg} src={aeyoungImg}></img>
+                </div>
+                <div className={styles.cardText}>카페</div>
+              </button>
+              <button
+                className={
+                  selectedOptions.includes('산')
+                    ? styles.cardActive
+                    : styles.card
+                }
+                onClick={() => handleOptionToggle('산')}
+              >
+                <div>
+                  <img className={styles.cardImg} src={aeyoungImg}></img>
+                </div>
+                <div className={styles.cardText}>산</div>
+              </button>
+              <button
+                className={
+                  selectedOptions.includes('축제')
+                    ? styles.cardActive
+                    : styles.card
+                }
+                onClick={() => handleOptionToggle('축제')}
+              >
+                <div>
+                  <img className={styles.cardImg} src={aeyoungImg}></img>
+                </div>
+                <div className={styles.cardText}>축제</div>
+              </button>
+              <button
+                className={
+                  selectedOptions.includes('문화 | 역사')
+                    ? styles.cardActive
+                    : styles.card
+                }
+                onClick={() => handleOptionToggle('문화 | 역사')}
+              >
+                <div>
+                  <img className={styles.cardImg} src={aeyoungImg}></img>
+                </div>
+                <div className={styles.cardText}>문화 | 역사</div>
+              </button>
+            </div>
+            <div className={styles.buttonBox} onClick={handleNextButton}>
               <button className={styles.nextButton}>다음</button>
             </div>
           </div>
