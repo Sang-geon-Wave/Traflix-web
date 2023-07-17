7a9f63d2dbf53cd4eb78487eed456b836ee5f74f
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2023-07-17 11:12:08 +0900

    Use Props Interface for CardCarouselComponent

--- traflix-web/src/components/CardCarouselComponent/index.tsx
+++ traflix-web/src/components/CardCarouselComponent/index.tsx
@@ -5,45 +5,18 @@ import stylesMobileDefault from './MobileDefault.module.scss';
 import { CardDataType } from '../../types/CardCarouselDataType';
 import { TouchEventHandler, useState, useRef, useEffect } from 'react';
 
-import svg_test1 from '../../assets/images/popular_spot_1.svg';
-import svg_test2 from '../../assets/images/popular_spot_2.svg';
-import dong from '../../assets/images/dongdaemun.png';
-import castle from '../../assets/images/castle.png';
-
-const CardCarouselComponent = () => {
+export interface PropsCardCarouselComponent {
+  cardData: CardDataType[];
+}
+const CardCarouselComponent: React.FunctionComponent<
+  PropsCardCarouselComponent
+> = ({ cardData }) => {
   const { screenClass } = useRootData(({ appStore }) => ({
     screenClass: appStore.screenClass.get(),
   }));
   const isDesktop = screenClass === 'xl';
 
   const styles = isDesktop ? stylesDesktopDefault : stylesMobileDefault;
-
-  const cardData: CardDataType[] = [
-    {
-      imgUrl: dong,
-      place: '1',
-      addr: '1',
-      info: '1',
-    },
-    {
-      imgUrl: castle,
-      place: '2',
-      addr: '2',
-      info: '2',
-    },
-    {
-      imgUrl: svg_test1,
-      place: '3',
-      addr: '3',
-      info: '3',
-    },
-    {
-      imgUrl: svg_test2,
-      place: '4',
-      addr: '4',
-      info: '4',
-    },
-  ];
 
   let touchStartX: number;
   let touchEndX: number;
--- traflix-web/src/pages/TestPopularSpotCardPage/index.tsx
+++ traflix-web/src/pages/TestPopularSpotCardPage/index.tsx
@@ -3,6 +3,12 @@ import stylesDesktopDefault from './DesktopDefault.module.scss';
 import stylesMobileDefault from './MobileDefault.module.scss';
 import CardCarouselComponent from '../../components/CardCarouselComponent';
 
+import svg_test1 from '../../assets/images/popular_spot_1.svg';
+import svg_test2 from '../../assets/images/popular_spot_2.svg';
+import png_test1 from '../../assets/images/dongdaemun.png';
+import png_test2 from '../../assets/images/castle.png';
+import { CardDataType } from '../../types/CardCarouselDataType';
+
 const TestPopularSpotCardPage = () => {
   const { screenClass } = useRootData(({ appStore }) => ({
     screenClass: appStore.screenClass.get(),
@@ -11,9 +17,35 @@ const TestPopularSpotCardPage = () => {
 
   const styles = isDesktop ? stylesDesktopDefault : stylesMobileDefault;
 
+  const cardData: CardDataType[] = [
+    {
+      imgUrl: png_test1,
+      place: '1',
+      addr: '1',
+      info: '1',
+    },
+    {
+      imgUrl: png_test2,
+      place: '2',
+      addr: '2',
+      info: '2',
+    },
+    {
+      imgUrl: svg_test1,
+      place: '3',
+      addr: '3',
+      info: '3',
+    },
+    {
+      imgUrl: svg_test2,
+      place: '4',
+      addr: '4',
+      info: '4',
+    },
+  ];
   return (
     <div className={styles.test}>
-      <CardCarouselComponent />
+      <CardCarouselComponent cardData={cardData} />
     </div>
   );
 };
