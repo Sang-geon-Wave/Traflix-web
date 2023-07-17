73574100b6d04d44d167d5be7a680c300021cd79
parkjs82 <54925443+parkjs82@users.noreply.github.com>
2023-07-17 15:29:06 +0900

    fix translate

--- traflix-web/src/components/CardCarouselComponent/index.tsx
+++ traflix-web/src/components/CardCarouselComponent/index.tsx
@@ -3,7 +3,7 @@ import useRootData from '../../hooks/useRootData';
 import stylesDesktopDefault from './DesktopDefault.module.scss';
 import stylesMobileDefault from './MobileDefault.module.scss';
 import { CardDataType } from '../../types/CardCarouselDataType';
-import { TouchEventHandler, useState, useRef, useEffect } from 'react';
+import { TouchEventHandler, useState } from 'react';
 
 export interface PropsCardCarouselComponent {
   cardData: CardDataType[];
@@ -23,77 +23,32 @@ const CardCarouselComponent: React.FunctionComponent<
 
   const [currIndex, setCurrIndex] = useState(0);
 
-  const carouselRef = useRef<HTMLUListElement>(null);
-  useEffect(() => {
-    if (carouselRef.current !== null) {
-      if (isDesktop)
-        carouselRef.current.style.transform = `translateX(-${
-          currIndex * 42
-        }vw)`;
-      else
-        carouselRef.current.style.transform = `translateX(-${
-          currIndex * 92
-        }vw)`;
-    }
-  }, [currIndex]);
-
-  const moveToNthSlide = (index: number) => {
-    setTimeout(() => {
-      setCurrIndex(index);
-      if (carouselRef.current !== null) {
-        carouselRef.current.style.transition = '';
-      }
-    }, 10);
-  };
   const prevButtonClicked = () => {
-    const newIndex = currIndex - 1;
-
-    if (newIndex === -1) {
-      if (isDesktop) moveToNthSlide(cardData.length - 2);
-      if (!isDesktop) moveToNthSlide(cardData.length - 1);
-    }
-
-    setCurrIndex((prev) => prev - 1);
-    if (carouselRef.current !== null) {
-      carouselRef.current.style.transition = 'all 0.5s ease-in-out';
-    }
+    setCurrIndex(() => {
+      if (currIndex === 0) {
+        if (isDesktop) return cardData.length - 2;
+        if (!isDesktop) return cardData.length - 1;
+      }
+      return currIndex - 1;
+    });
   };
   const nextButtonClicked = () => {
-    const newIndex = currIndex + 1;
-
-    if (isDesktop && newIndex === cardData.length - 1) moveToNthSlide(0);
-    if (!isDesktop && newIndex === cardData.length) moveToNthSlide(0);
-
-    setCurrIndex((prev) => prev + 1);
-    if (carouselRef.current !== null) {
-      carouselRef.current.style.transition = 'all 0.5s ease-in-out';
-    }
+    setCurrIndex(() => {
+      if (isDesktop && currIndex == cardData.length - 2) return 0;
+      if (!isDesktop && currIndex === cardData.length - 1) return 0;
+      return currIndex + 1;
+    });
   };
   const handleTouchStart: TouchEventHandler<HTMLDivElement> = (e) => {
     touchStartX = e.nativeEvent.touches[0].clientX;
   };
 
-  const handleTouchMove: TouchEventHandler<HTMLDivElement> = (e) => {
-    const currTouchX = e.nativeEvent.changedTouches[0].clientX;
-
-    if (carouselRef.current !== null) {
-      // if (isDesktop)
-      //   carouselRef.current.style.transform = `translateX(calc(-${
-      //     currIndex * 42
-      //   }vw`; // - ${touchStartX - currTouchX || 0}px)))`;
-      // if (!isDesktop)
-      //   carouselRef.current.style.transform = `translateX(calc(-${
-      //     currIndex * 92
-      //   }vw`; // - ${touchStartX - currTouchX || 0}px)))`;
-    }
-  };
-
   const handleTouchEnd: TouchEventHandler<HTMLDivElement> = (e) => {
     touchEndX = e.nativeEvent.changedTouches[0].clientX;
 
-    if (touchStartX >= touchEndX) {
+    if (touchStartX > touchEndX) {
       nextButtonClicked();
-    } else {
+    } else if (touchStartX > touchEndX) {
       prevButtonClicked();
     }
   };
@@ -102,9 +57,7 @@ const CardCarouselComponent: React.FunctionComponent<
     <div
       className={styles.container}
       onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
       onTouchEnd={handleTouchEnd}
-      // onScroll={}
     >
       <button className={styles.prev} onClick={() => prevButtonClicked()}>
         &lang;
@@ -112,7 +65,14 @@ const CardCarouselComponent: React.FunctionComponent<
       <button className={styles.next} onClick={() => nextButtonClicked()}>
         &rang;
       </button>
-      <ul className={styles.carousel} ref={carouselRef}>
+      <div
+        className={styles.carousel}
+        style={
+          isDesktop
+            ? { transform: `translateX(-${currIndex * 42}vw)` }
+            : { transform: `translateX(-${currIndex * 92}vw)` }
+        }
+      >
         {cardData.map((cardData) => (
           <PopularSpotCardComponent
             imgUrl={cardData.imgUrl}
@@ -121,7 +81,7 @@ const CardCarouselComponent: React.FunctionComponent<
             info={cardData.info}
           />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
