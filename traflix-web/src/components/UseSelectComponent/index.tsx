84ff11238739458f59d1ccb8818a4c011dbba8ab
xorud127 <xorud127@kookmin.ac.kr>
2023-08-02 20:53:02 +0900

    fix: button

--- traflix-web/src/components/UseSelectComponent/index.tsx
+++ traflix-web/src/components/UseSelectComponent/index.tsx
@@ -7,7 +7,6 @@ import { set } from 'mobx';
 
 const OPTION = {
   isOpen: false,
-  // selectedOptions: [] as string[],
 };
 
 const UseSelectComponent = ({}) => {
@@ -24,7 +23,6 @@ const UseSelectComponent = ({}) => {
       setModalOption((prev) => ({
         ...prev,
         isOpen,
-        // selectedOptions,
       }));
     },
     [modalOption],
---
+++ traflix-web/src/pages/MockPage copy/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import useRootData from '../../hooks/useRootData';
+import MockComponent from '../../components/MockComponent';
+import stylesDesktopDefault from './DesktopDefault.module.scss';
+import stylesMobileDefault from './MobileDefault.module.scss';
+import SelectComponent from '../../components/SelectComponent';
+import UseSelectComponent from '../../components/UseSelectComponent';
+
+const TestSelectPage = () => {
+  const { screenClass } = useRootData(({ appStore }) => ({
+    screenClass: appStore.screenClass.get(),
+  }));
+  const isDesktop = screenClass === 'xl';
+  const styles = isDesktop ? stylesDesktopDefault : stylesMobileDefault;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleShowModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <button onClick={handleShowModal}>test</button>
+      <SelectComponent isOpen={isOpen} handleShowModal={handleShowModal} />
+    </div>
+  );
+};
+
+export default TestSelectPage;
