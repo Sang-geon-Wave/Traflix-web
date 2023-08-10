9a1bfd55182ca0f559b274b9e2ff9144c448267d
xorud127 <xorud127@kookmin.ac.kr>
2023-08-10 22:07:02 +0900

    fix: del testpage

--- traflix-web/src/components/SelectComponent/index.tsx
+++ traflix-web/src/components/SelectComponent/index.tsx
@@ -42,8 +42,6 @@ const SelectComponent: React.FunctionComponent<PropsSelectComponent> = ({
     setSelectedOptions([]);
   };
 
-  const selectType = ['액티비티', '카페', '산', '축제', '문화 | 역사'];
-
   return (
     <div>
       {isOpen && (
