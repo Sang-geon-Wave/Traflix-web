98fc7bfed0fe7a8fa7d3dfa7c3b7026016b24748
SeokHyeon-Eom <eom2244@kookmin.ac.kr>
2023-09-08 13:03:41 +0900

    add: login fail event

--- traflix-web/src/components/Redirect/index.tsx
+++ traflix-web/src/components/Redirect/index.tsx
@@ -3,19 +3,34 @@ import useRootData from '../../hooks/useRootData';
 import { useNavigate } from 'react-router-dom';
 
 const Redirection = () => {
-  const { login } = useRootData(({ authStore }) => ({
-    login: authStore.login,
-  }));
+  const { login, handleLoginShow } = useRootData(
+    ({ authStore, loginModal }) => ({
+      login: authStore.login,
+      handleLoginShow: loginModal.handleLoginShow,
+    }),
+  );
 
   const code = new URL(window.location.href).searchParams.get('code');
   const navigate = useNavigate();
 
+  const success = () => {
+    alert(`환영합니다`);
+  };
+
+  const fail = () => {
+    alert(`로그인 실패`);
+    handleLoginShow();
+  };
+
   useEffect(() => {
     if (code) {
-      login(code);
+      const tryLogin = login(code);
       navigate('/');
-      alert(`환영합니다`);
-    } else alert('error');
+      tryLogin.then((state) => {
+        if (state) success();
+        else fail();
+      });
+    } else fail();
   }, []);
 
   return <div>로그인 중입니다.</div>;
