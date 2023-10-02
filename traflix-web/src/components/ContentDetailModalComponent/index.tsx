2cbef8462d8005d00d56e8ef7de72d884acca904
xorud127 <xorud127@kookmin.ac.kr>
2023-10-02 18:15:22 +0900

    add: intro detail

--- traflix-web/src/components/ContentDetailModalComponent/index.tsx
+++ traflix-web/src/components/ContentDetailModalComponent/index.tsx
@@ -48,10 +48,55 @@ const ContentDetailModalComponent: React.FunctionComponent<
       usefee: '이용요금',
       discountinfo: '할인정보',
     },
-    '32': {
-      pickup: '픽업 여부',
+    '15': {
+      placeinfo: '행사장 위치',
+      eventplace: '행사 장소',
+      eventstartdate: '행사 시작일',
+      eventenddate: '행사 종료일',
+      bookingplace: '예매처',
+      agelimit: '관람 가능연령',
     },
-    // 여기서 너가 각 관광지 타입마다 필요한 정보만 추가하면 끝남
+    '25': {
+      schedule: '코스 일정',
+      theme: '코스 테마',
+      infocentertourcourse: '문의 및 안내',
+    },
+    '28': {
+      openperiod: '개장기간',
+      restdateleports: '쉬는날',
+      usefeeleports: '입장료',
+      parkingleports: '주차시설',
+      expagerangeleports: '체험 가능연령',
+      infocenterleports: '문의 및 안내',
+    },
+    '32': {
+      accomcountlodging: '수용 가능인원',
+      checkintime: '입실 시간',
+      checkouttime: '퇴실 시간',
+      pickup: '픽업 서비스',
+      chkcooking: '객실내 취사 여부',
+      parkinglodging: '주차 시설',
+      reservationlodging: '예약 안내',
+      reservationurl: '예약안내 홈페이지',
+      infoventerlodging: '문의 및 안내',
+    },
+    '38': {
+      chkcreditcardshopping: '신용카드가능 정보',
+      opendateshopping: '개장일',
+      opentime: '영업시간',
+      restdateshopping: '쉬는날',
+      parkingshopping: '주차시설',
+      infocentershopping: '문의 및 안내',
+    },
+    '39': {
+      chkcreditcardfood: '신용카드가능 정보',
+      firstmenu: '대표 메뉴',
+      opendatefood: '개업일',
+      opentimefood: '영업시간',
+      restdatefood: '쉬는날',
+      packing: '포장 가능',
+      infocenterfood: '문의 및 안내',
+    },
   };
 
   return (
