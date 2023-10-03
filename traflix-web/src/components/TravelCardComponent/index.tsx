c11b149cc83ecd914130202391203677610676ca
xorud127 <xorud127@kookmin.ac.kr>
2023-10-03 15:25:50 +0900

    fix: moreinfo

--- traflix-web/src/components/TravelCardComponent/index.tsx
+++ traflix-web/src/components/TravelCardComponent/index.tsx
@@ -25,8 +25,8 @@ const TravelCardComponent: React.FunctionComponent<TravelCardDataType> = ({
 
   const setDetailModal = async () => {
     const { data } = await api.post('/search/contentDetail', {
-      //content_id: moreInfo,
-      content_id: 1886054, //2465071, //2851257, //132205
+      content_id: moreInfo,
+      // content_id: 2792802,
     });
 
     handleContentShow(data.detail);
